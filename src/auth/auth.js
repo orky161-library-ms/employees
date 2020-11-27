a1cba5d5660173e6657a680fa2d881b6d37fd22d
orky161 <orky161@gmail.com>
2020-11-27 23:57:48 +0200

    add auth

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -2,7 +2,6 @@ const axios = require("axios")
 
 const auth_mw =  (req, res, next) => {
     axios.get(`http://${process.env.AUTH_SERVICE}/api/auth/verify`,{headers: req.headers}).then(res =>{
-        console.log(res)
         req.decoded = {};
         req.decoded.email = res.data.decoded.email;
         req.decoded.id = res.data.decoded.id;
@@ -28,6 +27,11 @@ const permission_mw = (role) => (req, res, next) => {
 };
 
 const equalId_mw = (field) => (req, res, next) => {
+    if(req.decoded.role === "ADMIN"){
+        next();
+        return;
+    }
+
     if (req.params[field] === req.decoded[field]) {
         next();
         return;
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -4,15 +4,15 @@ const employeesLogic = new (require("../bl/employees"))()
 const {permission_mw, auth_mw, equalId_mw} = require("../auth/auth")
 const router = express.Router()
 
-router.get("/",[auth_mw] ,(async (req, res) => {
+router.get("/",[auth_mw, permission_mw("ADMIN")] ,(async (req, res) => {
     const employees = await employeesLogic.getEmployees()
     res.status(200).json({employees})
 }))
-router.post("/",(async (req, res) => {
+router.post("/", [auth_mw, permission_mw("ADMIN")], (async (req, res) => {
     const employee = await employeesLogic.addEmployee(req.body)
     res.status(200).json({employee})
 }))
-router.get("/:id",[auth_mw, permission_mw("ADMIN")],(async (req, res) => {
+router.get("/:id", [auth_mw, permission_mw("ADMIN")], (async (req, res) => {
     const employee = await employeesLogic.getEmployee(req.params.id)
     res.status(200).json({employee})
 }))
